8409a6c713d68e056c9e4f8bb0c8fdd49c41f84a
Craig Gumbley <craiggumbley@gmail.com>
2022-01-07 10:57:53 +0000

    Configure package template
    This commit makes all of the required changes to the package template
    provided by GitHub. We also remove a few things that we don't need and
    add some extra configs.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+};
